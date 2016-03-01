f37e26f40b35e8636eeea3cede343611e305438c
Jaff Parker <andreykamozin@gmail.com>
2016-03-01 15:25:37 -0500

    Added Socialite class

---
+++ Socialite.php
@@ -0,0 +1,13 @@
+<?php namespace Koodzo\Packages\Socialite;
+
+use Laravel\Socialite\SocialiteManager;
+
+class Socialite extends SocialiteManager
+{
+    public function createPinterestDriver()
+    {
+        $config = $this->app['config']['services.pinterest'];
+
+        return $this->buildProvider(Pinterest::class, $config);
+    }
+}
