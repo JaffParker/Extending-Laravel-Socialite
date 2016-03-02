9e41cb5c1c08918532c2f6ced0608ee8ddbf5856
Jaff Parker <andreykamozin@gmail.com>
2016-03-01 23:12:56 -0500

    Changed the service provider
    Instead of just copy-pasting the entire provider, I simply extended it. Was stupid not to do like that in the first place.

--- SocialiteServiceProvider.php
+++ SocialiteServiceProvider.php
@@ -1,36 +1,14 @@
 <?php namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Laravel\Socialite\SocialiteServiceProvider;
 use App\Socialite;
 
-class SocialiteServiceProvider extends ServiceProvider
+class SocialiteServiceProvider extends SocialiteServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->app->singleton('Laravel\Socialite\Contracts\Factory', function ($app) {
             return new Socialite($app);
         });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['Laravel\Socialite\Contracts\Factory'];
     }
 }
